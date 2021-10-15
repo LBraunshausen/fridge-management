c09eceb52961797f76ed0f270196a79892fc8d12
LBraunshausen <lars.braunshausen@t-online.de>
2021-10-15 11:16:05 +0200

    Renamed About-Page/ViewModel to FridgeItems-Page/ViewModel

---
+++ fridge-management/ViewModels/FridgeItemsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace fridge_management.ViewModels
+{
+    public class FridgeItemsViewModel : BaseViewModel
+    {
+        public FridgeItemsViewModel()
+        {
+            Title = "Kühlschrankinhalt";
+        }        
+    }
+}
