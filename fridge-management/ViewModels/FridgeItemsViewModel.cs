1d6aa651272af13a45a8a23c9f93ecc45028bbe5
LBraunshausen <lars.braunshausen@t-online.de>
2021-10-17 15:22:39 +0200

    Added AddCommand

--- fridge-management/ViewModels/FridgeItemsViewModel.cs
+++ fridge-management/ViewModels/FridgeItemsViewModel.cs
@@ -1,15 +1,28 @@
-﻿using System;
+﻿using fridge_management.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+
 
 namespace fridge_management.ViewModels
 {
     public class FridgeItemsViewModel : BaseViewModel
     {
+        public ObservableCollection<FridgeItem> FridgeItem { get; set; }
+        public Command AddCommand { get; }
         public FridgeItemsViewModel()
         {
             Title = "Kühlschrankinhalt";
-        }        
+
+            AddCommand = new Command(Add);
+        }
+
+        private async void Add()
+        {
+            
+        }
     }
 }
