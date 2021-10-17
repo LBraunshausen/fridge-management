4164a5618054bd08ee654bd8f078ed6ffa0e3ebd
LBraunshausen <lars.braunshausen@t-online.de>
2021-10-17 14:48:06 +0200

    Added ExpirationDate-Property

--- fridge-management/Models/Item.cs
+++ fridge-management/Models/Item.cs
@@ -6,6 +6,6 @@ namespace fridge_management.Models
     {
         public string Id { get; set; }
         public string Text { get; set; }
-        public string Description { get; set; }
+        public DateTime ExpirationDate{ get; set; }
     }
 }
