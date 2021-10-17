2cf25d4106abf12d243c03f10604a657311e6dc2
LBraunshausen <lars.braunshausen@t-online.de>
2021-10-17 15:10:49 +0200

    Added Primarykey-Attribute to Id property

--- fridge-management/Models/FridgeItem.cs
+++ fridge-management/Models/FridgeItem.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using SQLite;
+using System;
 
 namespace fridge_management.Models
 {
     public class FridgeItem
     {
+        [PrimaryKey, AutoIncrement]
         public string Id { get; set; }
         public string Text { get; set; }
         public DateTime ExpirationDate{ get; set; }
