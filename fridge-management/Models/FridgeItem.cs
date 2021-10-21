c50211bd48eda05d271209ac0245ae28b73baf0f
LBraunshausen <lars.braunshausen@t-online.de>
2021-10-21 16:06:53 +0200

    Added Amount Property

--- fridge-management/Models/FridgeItem.cs
+++ fridge-management/Models/FridgeItem.cs
@@ -8,6 +8,7 @@ namespace fridge_management.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Text { get; set; }
-        public DateTime ExpirationDate{ get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public int Amount { get; set; }
     }
 }
