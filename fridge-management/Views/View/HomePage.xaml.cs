88613b60b7ac439abc0545c68dde43887ced5cec
LBraunshausen <lars.braunshausen@t-online.de>
2021-11-06 12:31:05 +0100

    Added Home page

---
+++ fridge-management/Views/View/HomePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace fridge_management.Views
+{	
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
