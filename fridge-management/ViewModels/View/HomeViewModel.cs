298caecfb82c3c1fb4c87251b465332c095a9fbd
LBraunshausen <lars.braunshausen@t-online.de>
2021-11-06 12:30:52 +0100

    Added ViewModel

---
+++ fridge-management/ViewModels/View/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using fridge_management.Views;
+
+namespace fridge_management.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+
+    }
+}
