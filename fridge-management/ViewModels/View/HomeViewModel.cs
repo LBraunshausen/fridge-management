fed189f597c17f45b4ef4b74335ec9526d20c843
LBraunshausen <lars.braunshausen@t-online.de>
2021-11-10 14:43:23 +0100

    Added base structure

--- fridge-management/ViewModels/View/HomeViewModel.cs
+++ fridge-management/ViewModels/View/HomeViewModel.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using fridge_management.Models;
+using fridge_management.Services;
 using fridge_management.Views;
+using MvvmHelpers;
+using Xamarin.Forms;
 
 namespace fridge_management.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
+        public ObservableRangeCollection<FridgeItem> FridgeItems { get; set; }
 
+        private FridgeItem selectedItem;
+        public FridgeItem SelectedItem
+        {
+            get => selectedItem;
+            set
+            {
+                if (value == selectedItem)
+                    return;
+                selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public HomeViewModel()
+        {
+            Title = "Kühlschrankinhalt";
+            FridgeItems = new ObservableRangeCollection<FridgeItem>();
+            Load();
+        }
+
+
+        public async Task Load()
+        {
+            IsBusy = true;
+            FridgeItems.Clear();
+            var fridgeItems = await BaseService<FridgeItem>.GetItems();
+            FridgeItems.AddRange(fridgeItems);
+            IsBusy = false;
+        }
     }
 }
