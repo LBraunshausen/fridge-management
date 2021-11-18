a4352f7d78c35d73cb9447eba209e169ca786089
LBraunshausen <lars.braunshausen@t-online.de>
2021-11-18 14:15:01 +0100

    Implemented NewFridgeItemViewmodel in FridgeItemsViewmodel

--- fridge-management/ViewModels/View/FridgeItemsViewModel.cs
+++ fridge-management/ViewModels/View/FridgeItemsViewModel.cs
@@ -2,6 +2,7 @@
 using fridge_management.Services;
 using fridge_management.Views;
 using MvvmHelpers;
+using System;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -11,12 +12,13 @@ namespace fridge_management.ViewModels
     public class FridgeItemsViewModel : BaseViewModel
     {
         public ObservableRangeCollection<FridgeItem> FridgeItems { get; set; }
+        public Command OpenAddPageCommand { get; }
         public Command AddCommand { get; }
         public Command RemoveCommand { get; }
         public Command EditCommand { get; }
 
 
-        private FridgeItem selectedItem;
+        public FridgeItem selectedItem;
         public FridgeItem SelectedItem
         {
             get => selectedItem;
@@ -26,16 +28,57 @@ namespace fridge_management.ViewModels
                     return;
                 selectedItem = value;
                 OnPropertyChanged();
+                
             }
         }
+
+        public string Text
+        {
+            get => selectedItem.Text;
+            set
+            {
+                if (value == selectedItem.Text)
+                    return;
+                selectedItem.Text = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime ExpirationDate
+        {
+            get => selectedItem.ExpirationDate;
+            set
+            {
+                if (value == selectedItem.ExpirationDate)
+                    return;
+                selectedItem.ExpirationDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Amount
+        {
+            get => selectedItem.Amount;
+            set
+            {
+                if (value == selectedItem.Amount)
+                    return;
+                selectedItem.Amount = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         public FridgeItemsViewModel()
         {
             Title = "Kühlschrankinhalt";
+            OpenAddPageCommand = new Command(OpenAddPage);
             AddCommand = new Command(Add);
             RemoveCommand = new Command(Remove);
             EditCommand = new Command(Edit);
             FridgeItems = new ObservableRangeCollection<FridgeItem>();
+            selectedItem = new FridgeItem();
+            ExpirationDate = DateTime.Now;
             Load();
 
             MessagingCenter.Subscribe<object, string>("MyApp", "Update",
@@ -46,10 +89,19 @@ namespace fridge_management.ViewModels
               );
         }
 
-        private async void Add()
+        private async void OpenAddPage()
         {
             await Shell.Current.GoToAsync(nameof(NewFridgeItemPage));
         }
+        public async void Add()
+        {
+            await BaseService<FridgeItem>.Add(selectedItem);
+            MessagingCenter.Send<object, string>("MyApp", "Update", "List");
+            await Application.Current.MainPage.Navigation.PopAsync();
+        }
+
+
+
 
         private async void Remove()
         {
