10b9ed6c3a04423cefef73b3aa7cf78619016d67
LBraunshausen <lars.braunshausen@t-online.de>
2021-11-26 12:20:21 +0100

    Edited Id from int to guid

--- fridge-management/Models/FridgeItem.cs
+++ fridge-management/Models/FridgeItem.cs
@@ -9,7 +9,7 @@ namespace fridge_management.Models
     public class FridgeItem : IModel
     {
         [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Text { get; set; }
         public DateTime ExpirationDate { get; set; }
         public int Amount { get; set; }
