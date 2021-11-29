47551602ed889bf679d1e91cd8e3e5d3fa80d4cb
LBraunshausen <lars.braunshausen@t-online.de>
2021-11-29 12:21:06 +0100

    Added TestAdd test

---
+++ fridge_management_Test/FridgeItemsViewModelTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using fridge_management.ViewModels;
+using fridge_management.Models;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using System.Collections;
+
+
+namespace fridge_management_Test
+{
+    [TestClass]
+    public class FridgeItemsViewModelTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            FridgeItemsViewModel vm = new FridgeItemsViewModel();
+
+            if (vm.AddCommand.CanExecute(null))
+                vm.AddCommand.Execute(null);
+            
+        }
+    }
+}
