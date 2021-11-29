bc821a1b7a3a43aa22ee6730cf57ab8b999e8845
tomu1996 <82444418+tomu1996@users.noreply.github.com>
2021-11-29 13:37:26 +0100

    Add Comments for the shopping list models

--- fridge-management/Models/ShoppingList.cs
+++ fridge-management/Models/ShoppingList.cs
@@ -4,10 +4,19 @@ using System.Collections.Generic;
 
 namespace fridge_management.Models
 {
+    /// <summary>
+    ///     Model which contains data of shopping lists
+    /// </summary>
     public class ShoppingList :  IModel
     {
+        /// <summary>
+        ///     Contains an global unique identifier
+        /// </summary>
         [PrimaryKey, AutoIncrement]
         public Guid Id { get; set; }
+        /// <summary>
+        /// Contains the name of the shopping list
+        /// </summary>
         public string Text { get; set; }
     }
 }
--- fridge-management/Models/ShoppingListItem.cs
+++ fridge-management/Models/ShoppingListItem.cs
@@ -4,13 +4,31 @@ using System.Collections.Generic;
 
 namespace fridge_management.Models
 {
+    /// <summary>
+    ///     Model which contains a ShoppingListItem
+    /// </summary>
     public class ShoppingListItem : IModel
     {
+        /// <summary>
+        /// Contains an global unique identifier
+        /// </summary>
         [PrimaryKey, AutoIncrement]
         public Guid Id { get; set; }
+        /// <summary>
+        /// Contains the name of the item
+        /// </summary>
         public string Text { get; set; }
+        /// <summary>
+        /// Contains the name of the item
+        /// </summary>
         public DateTime ExpirationDate { get; set; }
+        /// <summary>
+        /// Contains the amount of the item
+        /// </summary>
         public int Amount { get; set; }
+        /// <summary>
+        /// Contains the number of the assigned shopping list
+        /// </summary>
         public int ShoppingListNummer { get; set; }
     }
 }
