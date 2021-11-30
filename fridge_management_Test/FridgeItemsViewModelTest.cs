ad8ac3034bfe8d146b47b22c670aacc714048542
LBraunshausen <lars.braunshausen@t-online.de>
2021-11-30 15:10:01 +0100

    Added comments

--- fridge_management_Test/FridgeItemsViewModelTest.cs
+++ fridge_management_Test/FridgeItemsViewModelTest.cs
@@ -6,21 +6,47 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using System.Collections;
-
+using fridge_management.Services;
+using Xamarin.Forms;
+using fridge_management.Views;
+using fridge_management;
 
 namespace fridge_management_Test
 {
     [TestClass]
     public class FridgeItemsViewModelTest
     {
+        /// <summary>
+        ///     Testmethod to test FridgeItemsViewModels Add method
+        /// </summary>
         [TestMethod]
         public void TestAdd()
-        {
-            FridgeItemsViewModel vm = new FridgeItemsViewModel();
+        {                        
+            FridgeItemsViewModel vm = new FridgeItemsViewModel();            
+
+            if (vm.OpenAddPageCommand.CanExecute(null))
+                vm.OpenAddPageCommand.Execute(null);
+
+            vm.SelectedItem = new FridgeItem()
+            {                
+                Text = "Testprodukt",
+                Amount = 1,
+                ExpirationDate = DateTime.Now
+            };
 
             if (vm.AddCommand.CanExecute(null))
                 vm.AddCommand.Execute(null);
-            
+
+            Task task = Task.Run(async () =>
+            {
+                await BaseService<FridgeItem>.Add(vm.SelectedItem);
+                FridgeItem fridgeItem = null;
+
+                fridgeItem = await BaseService<FridgeItem>.GetById(vm.SelectedItem.Id);
+
+                Assert.AreEqual(vm.SelectedItem.Id, fridgeItem.Id, "not equal");
+            });
+            task.Wait();
         }
     }
 }
