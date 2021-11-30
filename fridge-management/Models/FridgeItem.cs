1c5c30a9e2939b72bb4cad14e465d4b93f10a20f
LBraunshausen <lars.braunshausen@t-online.de>
2021-11-30 15:26:04 +0100

    Added comments

--- fridge-management/Models/FridgeItem.cs
+++ fridge-management/Models/FridgeItem.cs
@@ -8,10 +8,22 @@ namespace fridge_management.Models
     /// </summary>
     public class FridgeItem : IModel
     {
+        /// <summary>
+        /// Contains an global unique identifier
+        /// </summary>
         [PrimaryKey, AutoIncrement]
         public Guid Id { get; set; }
+        /// <summary>
+        /// Contains the name of a FridgeItem
+        /// </summary>
         public string Text { get; set; }
+        /// <summary>
+        /// Contains the expiration date of a FridgeItem
+        /// </summary>
         public DateTime ExpirationDate { get; set; }
+        /// <summary>
+        /// Contains the amount date of a FridgeItem
+        /// </summary>
         public int Amount { get; set; }
     }
 }
--- fridge-management/Models/IModel.cs
+++ fridge-management/Models/IModel.cs
@@ -6,6 +6,9 @@ namespace fridge_management.Models
 {
     public interface IModel
     {
+        /// <summary>
+        ///     Contains an global unique identifier
+        /// </summary>
         Guid Id { get; set; }
     }
 }
