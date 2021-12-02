a0f4f50e17a13e38c95e51026a948b1c4a0aa114
LBraunshausen <lars.braunshausen@t-online.de>
2021-12-02 14:57:01 +0100

    Added barcode scanner implementation

---
+++ fridge-management/Views/View/BarcodeScannerPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace fridge_management.Views
+{	
+    public partial class BarcodeScannerPage : ContentPage
+    {
+        public BarcodeScannerPage()
+        {
+            InitializeComponent();
+        }        
+    }
+}
